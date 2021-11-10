128a161c20c0191db2a5789bb63afd1b8ece71fb
sh0d4n <pertgubanov37@gmail.com>
2021-11-10 10:23:04 +0300

    add bugs

--- 7_sem/home/2.sfml_balls/balls.cpp
+++ 7_sem/home/2.sfml_balls/balls.cpp
@@ -4,35 +4,40 @@
 #include <cstdlib>
 #include <ctime>
 
-struct Ball {
+struct ball_t {
     float        radius;
     sf::Vector2f position;
     sf::Vector2f velocity;
 };
 
-void boundary_conditions_tor (sf::Vector2f &position, std::pair < float, float > bound);
+void conditions_tor     (ball_t &ball, std::pair < float, float > bound);
+void conditions_reflect (ball_t &ball, std::pair < float, float > bound);
 
 int main() {
-    srand(time(0));
+    srand(time(nullptr));
     
-    const int width   = 800;
-    const int height  = 600;
-    const int n_balls = 10;
+    const int width     = 800;
+    const int height    = 600;
+    const int n_balls   = 10;
     const float delta_t = 0.1;
 
     sf::RenderWindow window(sf::VideoMode(width, height), "My window");
-    window.setFramerateLimit(24);
+    window.setFramerateLimit(60);
 
     sf::CircleShape circle(50.0f);
     circle.setFillColor({200, 216, 200});
 
-    std::vector<Ball> balls;
+    std::vector<ball_t> balls;
     balls.resize(n_balls);
     for (int i = 0; i < n_balls; i++) {
         balls[i].radius = 4 + rand() % 8;
         balls[i].position = {(float)(rand() % width), (float)(rand() % height)};
         balls[i].velocity = {(float)(rand() % 100 - 50), (float)(rand() % 100 - 50)};
     }
+
+    balls[9].radius = 50;
+    balls[9].position = {float(width / 2), float(height / 2)};
+    balls[9].velocity = {20, 20} ;
 
     while (window.isOpen()) {
         sf::Event event;
@@ -45,10 +50,10 @@ int main() {
 
         for (int i = 0; i < n_balls; i++) {
             balls[i].position += balls[i].velocity * delta_t;
-			boundary_conditions_tor(balls[i].position, {width, height});
             circle.setRadius(balls[i].radius);
             circle.setOrigin(balls[i].radius, balls[i].radius);
             circle.setPosition(balls[i].position);
+            conditions_reflect(balls[i], {width, height});
             
             window.draw(circle);
         }
@@ -59,10 +64,30 @@ int main() {
     return 0;
 }
 
-void boundary_conditions_tor (sf::Vector2f &position, std::pair < float, float > bound) {
-	if (position.y > bound.second) position.y -= bound.second;
-	else if (position.y < 0)       position.y += bound.second;
+void conditions_tor     (ball_t &ball, std::pair < float, float > bound) {
+	if (ball.position.y > bound.second) ball.position.y -= bound.second;
+	else if (ball.position.y < 0)       ball.position.y += bound.second;
 
-	if (position.x > bound.first) position.x -= bound.first ;
-	else if (position.x < 0)      position.x += bound.first ;
+	if (ball.position.x > bound.first) ball.position.x -= bound.first ;
+	else if (ball.position.x < 0)      ball.position.x += bound.first ;
+}
+
+void conditions_reflect (ball_t &ball, std::pair < float, float > bound) {
+    if (ball.position.y + ball.radius > bound.second) { 
+        ball.position.y = bound.second - ball.radius;
+        ball.velocity.y = -ball.velocity.y;
+    }
+    else if (ball.position.y - ball.radius < 0) {
+        ball.position.y = ball.radius;
+        ball.velocity.y = -ball.velocity.y;
+    }
+
+    if (ball.position.x + ball.radius > bound.first) { 
+        ball.position.x = bound.first - ball.radius;
+        ball.velocity.x = -ball.velocity.x;
+    }
+    else if (ball.position.x - ball.radius < 0) {
+        ball.position.x = ball.radius;
+        ball.velocity.x = -ball.velocity.x;
+    }
 }
